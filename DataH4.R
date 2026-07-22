79a380beb626508e7545d1fdb8ea34d98b2a9cf6
Mritshasridhar <mritshasridhar21@gmail.com>
2026-07-22 11:08:38 +0530

    Delete DataH4.R

--- DataH4.R
+++
@@ -1,7 +0,0 @@
-dept <- c("Marketing", "Sales", "HR", "Finance", "IT")
-employees <- c(50, 60, 30, 20, 40)
-pie(employees,
-    labels = paste(dept, employees),
-    col = rainbow(length(dept)),
-    main = "Employee Distribution by Department")
-cat("Employees in Marketing Department =", employees[1])
